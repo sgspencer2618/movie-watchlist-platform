2032e814df489f55ffca7518151053f54fbbb253
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-10-30 10:52:28 -0400

    finished framework for Movie entity

--- src/entity/Movie.java
+++ src/entity/Movie.java
@@ -1,2 +1,28 @@
-package entity;public class Movie {
+package entity;
+
+import java.util.List;
+
+public class Movie {
+
+    private final String title;
+    private final String summary;
+    private final List<Integer> ratings;
+
+    public Movie(String title, String summary, List<Integer> ratings) {
+        this.title = title;
+        this.summary = summary;
+        this.ratings = ratings;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public List<Integer> getRatings() {
+        return ratings;
+    }
 }
