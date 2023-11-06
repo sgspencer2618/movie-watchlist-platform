9f0572aaac41377f8b282445aeaf3fe459f04bb7
Alex732004 <alex.yao@mail.utoronto.ca>
2023-11-05 21:02:55 -0500

    more skeleton for data access

--- src/data_access/UserRatingAccessObject.java
+++ src/data_access/UserRatingAccessObject.java
@@ -1,7 +1,13 @@
 package data_access;
-public class UserRatingAccessObject{
+import use_case.search.SearchUserRatingsDataAccessInterface;
+
+public class UserRatingAccessObject implements SearchUserRatingsDataAccessInterface{
 
     public UserRatingAccessObject() {
 
+    }
+
+    public String getUserRating() {
+        return "";
     }
 }
