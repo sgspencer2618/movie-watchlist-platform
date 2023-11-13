c3c904a920bc6dd3c4d5a662a22dd6b73e56b953
feliser <bonopoape@gmail.com>
2023-11-13 09:48:05 -0500

    Add MoveInfoInputData.java

---
+++ src/use_case/movie_info/MovieInfoInputData.java
@@ -0,0 +1,10 @@
+package use_case.movie_info;
+
+public class MovieInfoInputData {
+
+    private String imdbID;
+
+    public MovieInfoInputData(String imdbID) {
+        this.imdbID = imdbID;
+    }
+}
