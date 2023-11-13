cbb1f41608cafbac07bd9fd9b3d7685806b8d11b
rohanbrohan <rohanbrohan@gmail.com>
2023-11-13 15:54:44 -0500

    defined methods in ApiInterface2

--- src/utility/ApiInterface2.java
+++ src/utility/ApiInterface2.java
@@ -1,2 +1,11 @@
-package utility;public interface ApiInterface2 {
+package utility;
+
+import entity.Movie;
+
+import java.util.List;
+
+public interface ApiInterface2 {
+    public List<Movie> getSearch(String search, int page);
+
+    public Movie getMovie(String imdbID);
 }
