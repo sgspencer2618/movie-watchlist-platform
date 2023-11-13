74ed1b1b43e249221e81b0a65e38d12879593228
Alex732004 <alex.yao@mail.utoronto.ca>
2023-11-13 10:07:46 -0500

    fixed import and packages for Remove and Update Ratings

--- src/interface_adapters/remove_rating/RemoveRatingController.java
+++ src/interface_adapters/remove_rating/RemoveRatingController.java
@@ -1,4 +1,7 @@
 package interface_adapters.remove_rating;
 
+import use_case.remove_rating.RemoveRatingInputBoundary;
+import use_case.remove_rating.RemoveRatingInputData;
+
 public class RemoveRatingController {
 }
