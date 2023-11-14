99f1d552df830810a3ef1975c5ea9d4f640267b8
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-14 00:06:39 -0500

    created an implemented watchlist data access object class

--- src/data_access/WatchlistAccessObject.java
+++ src/data_access/WatchlistAccessObject.java
@@ -1,6 +1,14 @@
 package data_access;
 
+import entity.Movie;
 import use_case.add_to_watchlist.AddToWatchlistDataAccessInterface;
 
+import java.util.List;
+
 public class WatchlistAccessObject implements AddToWatchlistDataAccessInterface {
+
+    @Override
+    public List<Movie> getWatchlist(List watchlist) {
+        return null;
+    }
 }
