e092614744320398d610852c86e3c76db92e1704
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-14 00:05:53 -0500

    implemented code where possible for add to watchlist use case

--- src/use_case/add_to_watchlist/AddToWatchlistDataAccessInterface.java
+++ src/use_case/add_to_watchlist/AddToWatchlistDataAccessInterface.java
@@ -1,4 +1,8 @@
 package use_case.add_to_watchlist;
 
+import entity.Movie;
+import java.util.List;
+
 public interface AddToWatchlistDataAccessInterface {
+    public List<Movie> getWatchlist(List watchlist);
 }
--- src/use_case/add_to_watchlist/AddToWatchlistInputData.java
+++ src/use_case/add_to_watchlist/AddToWatchlistInputData.java
@@ -1,4 +1,15 @@
 package use_case.add_to_watchlist;
 
+import entity.Movie;
+
 public class AddToWatchlistInputData {
+    private final Movie movie;
+
+    public AddToWatchlistInputData(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
 }
