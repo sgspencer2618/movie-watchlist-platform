22c7604daef0b0b9e022ec0f0eb441ff5d610d63
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-17 18:05:44 -0500

    implemented interface adapters for get_watchlist use case

--- src/interface_adapters/get_watchlist/GetWatchlistPresenter.java
+++ src/interface_adapters/get_watchlist/GetWatchlistPresenter.java
@@ -1,4 +1,16 @@
 package interface_adapters.get_watchlist;
 
-public class GetWatchlistPresenter {
+import use_case.get_watchlist.GetWatchlistOutputBoundary;
+import use_case.get_watchlist.GetWatchlistOutputData;
+
+public class GetWatchlistPresenter implements GetWatchlistOutputBoundary {
+    @Override
+    public void prepareSuccessView(GetWatchlistOutputData getWatchlistOutputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
