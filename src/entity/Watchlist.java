e2f7b2a1ecb74ecb2268fafc00fa757d176960ff
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-17 15:58:36 -0500

    added watchlist attribute to watchlist entity and created getter method

--- src/entity/Watchlist.java
+++ src/entity/Watchlist.java
@@ -3,10 +3,12 @@ package entity;
 public class Watchlist {
     private final int userID;
     private final int watchlistID; //independent watchlist ID in case users will be able to create more than one watchlist in the future
+    private final String[] watchlist;
 
-    public Watchlist(int userID, int watchlistID) {
+    public Watchlist(int userID, int watchlistID, String[] watchlist) {
         this.userID = userID;
         this.watchlistID = watchlistID;
+        this.watchlist = watchlist;
     }
 
     public int getUserID() {
@@ -15,5 +17,9 @@ public class Watchlist {
 
     public int getWatchlistID() {
         return watchlistID;
+    }
+
+    public String[] getWatchlist() {
+        return watchlist;
     }
 }
