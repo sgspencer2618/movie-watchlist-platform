7f10eeb47f56d89f82c4c6daa53f28275c06c4ec
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-18 14:06:40 -0500

    made watchlist DAO implement get watchlist data access interface

--- src/data_access/WatchlistAccessObject.java
+++ src/data_access/WatchlistAccessObject.java
@@ -3,8 +3,9 @@ package data_access;
 import entity.User;
 import entity.Watchlist;
 import use_case.add_to_watchlist.AddToWatchlistDataAccessInterface;
+import use_case.get_watchlist.GetWatchlistDataAccessInterface;
 
-public class WatchlistAccessObject implements AddToWatchlistDataAccessInterface {
+public class WatchlistAccessObject implements AddToWatchlistDataAccessInterface, GetWatchlistDataAccessInterface {
 
     @Override
     public Watchlist getWatchlist(String user) {
