01f3dbba1c0be71b717af94b4f745e5733701593
feliser <bonopoape@gmail.com>
2023-11-17 19:37:36 -0500

    Add detailed movie entity for getMovieInfo use case

---
+++ src/entity/MovieDetailed.java
@@ -0,0 +1,67 @@
+package entity;
+
+public class MovieDetailed {
+
+    private String title;
+    private String summary;
+
+    // PG - 13, G, etc... (not enjoyment rating)
+    private String rating;
+    private String imdbScore, rottenTomatoesScore, metacriticScore;
+    private String director, actors;
+    private int year, runtime;
+
+    public MovieDetailed(String title, String summary, String rating, String imdbScore, String rottenTomatoesScore,
+                         String metacriticScore, String director, String actors, int year, int runtime) {
+        this.title = title;
+        this.summary = summary;
+        this.rating = rating;
+        this.imdbScore = imdbScore;
+        this.rottenTomatoesScore = rottenTomatoesScore;
+        this.metacriticScore = metacriticScore;
+        this.director = director;
+        this.actors = actors;
+        this.year = year;
+        this.runtime = runtime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getImdbScore() {
+        return imdbScore;
+    }
+
+    public String getRottenTomatoesScore() {
+        return rottenTomatoesScore;
+    }
+
+    public String getMetacriticScore() {
+        return metacriticScore;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+}
