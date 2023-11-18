55ef0df97d80c6df6a9935bf6b60232d166bd86d
feliser <bonopoape@gmail.com>
2023-11-17 20:35:11 -0500

    Update ApiInterface2 to use new MovieDetailed class

--- src/utility/ApiInterface2.java
+++ src/utility/ApiInterface2.java
@@ -1,11 +1,12 @@
 package utility;
 
 import entity.Movie;
+import entity.MovieDetailed;
 
 import java.util.List;
 
 public interface ApiInterface2 {
     public List<Movie> getSearch(String search, int page);
 
-    public Movie getMovie(String imdbID);
+    public MovieDetailed getMovie(String imdbID);
 }
