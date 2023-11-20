8437a99e240b1f6fd451e46786ae0376f486f68a
Alex732004 <alex.yao@mail.utoronto.ca>
2023-11-20 15:01:29 -0500

    interface adapters for remove rating

--- src/interface_adapters/remove_rating/RemoveRatingPresenter.java
+++ src/interface_adapters/remove_rating/RemoveRatingPresenter.java
@@ -1,4 +1,33 @@
 package interface_adapters.remove_rating;
 
-public class RemoveRatingPresenter {
+import interface_adapters.movie_info.MovieInfoState;
+import use_case.remove_rating.RemoveRatingOutputData;
+import use_case.remove_rating.RemoveRatingDataAccessInterface;
+import use_case.remove_rating.RemoveRatingOutputBoundary;
+import interface_adapters.ViewManagerModel;
+
+public class RemoveRatingPresenter implements RemoveRatingOutputBoundary {
+    private final RemoveRatingViewModel removeRatingViewModel;
+
+    private ViewManagerModel viewManagerModel;
+
+    public RemoveRatingPresenter (ViewManagerModel viewManagerModel,
+                           RemoveRatingViewModel removeRatingViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.removeRatingViewModel = removeRatingViewModel;
+
+    }
+    @Override
+    public void prepareSuccessView(RemoveRatingOutputData deletedRating) {
+
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        RemoveRatingState removeRatingState = removeRatingViewModel.getState();
+        removeRatingState.setRemoveRatingError(error);
+        removeRatingState.setRemovedRating(false);
+        removeRatingViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapters/remove_rating/RemoveRatingState.java
+++ src/interface_adapters/remove_rating/RemoveRatingState.java
@@ -1,4 +1,27 @@
 package interface_adapters.remove_rating;
 
 public class RemoveRatingState {
+    private String removeRatingError = null;
+    private boolean removedRating;
+    public RemoveRatingState(RemoveRatingState copy) {
+        removeRatingError = copy.removeRatingError;
+        removedRating = copy.removedRating;
+    }
+
+    public RemoveRatingState() {
+    }
+
+    public String getRemoveRatingError() {
+        return removeRatingError;
+    }
+
+    public void setRemoveRatingError(String removeRatingError) {
+
+        this.removeRatingError = removeRatingError;
+    }
+    public boolean getRemovedRating(){return removedRating;}
+
+    public void setRemovedRating(boolean removedRating) {
+        this.removedRating = removedRating;
+    }
 }
