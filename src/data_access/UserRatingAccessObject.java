bd9f3f82c1ded7d323dfb0b8750c0cb6fdfaa58f
Alex732004 <alex.yao@mail.utoronto.ca>
2023-11-20 11:53:25 -0500

    added some methods that will need to be implemented

--- src/data_access/UserRatingAccessObject.java
+++ src/data_access/UserRatingAccessObject.java
@@ -2,16 +2,21 @@ package data_access;
 import entity.Movie;
 import org.json.JSONObject;
 import use_case.get_ratings.GetRatingsDataAccessInterface;
+import use_case.remove_rating.RemoveRatingDataAccessInterface;
 import use_case.search.SearchUserRatingsDataAccessInterface;
+import use_case.update_rating.UpdateRatingDataAccessInterface;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class UserRatingAccessObject implements SearchUserRatingsDataAccessInterface, GetRatingsDataAccessInterface {
+public class UserRatingAccessObject implements SearchUserRatingsDataAccessInterface, GetRatingsDataAccessInterface,
+        RemoveRatingDataAccessInterface, UpdateRatingDataAccessInterface {
 
     public UserRatingAccessObject() {
 
     }
+    public void removeRating(String username, String move_id){}
+    public void updateRating(String username, String move_id, int newRating){}
     public int getUserRating(String user, String movieID){
         return 1;
     }
