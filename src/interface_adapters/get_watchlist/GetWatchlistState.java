f3b703abf68240223e7ce1b011fccdd519b52693
Sean Spencer <seanspencer@Seans-MacBook-Air.local>
2023-11-20 10:45:51 -0500

    implemented get watchlist view model and state

--- src/interface_adapters/get_watchlist/GetWatchlistState.java
+++ src/interface_adapters/get_watchlist/GetWatchlistState.java
@@ -1,4 +1,32 @@
 package interface_adapters.get_watchlist;
 
+import entity.Watchlist;
+
 public class GetWatchlistState {
+
+    private Watchlist watchlist;
+    private String user;
+
+    public GetWatchlistState(GetWatchlistState copy) {
+        this.watchlist = copy.watchlist;
+        this.user = copy.user;
+    }
+
+    GetWatchlistState() {}
+
+    public Watchlist getWatchlist() {
+        return watchlist;
+    }
+
+    public void setWatchlist(Watchlist watchlist) {
+        this.watchlist = watchlist;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
--- src/interface_adapters/get_watchlist/GetWatchlistViewModel.java
+++ src/interface_adapters/get_watchlist/GetWatchlistViewModel.java
@@ -1,4 +1,35 @@
 package interface_adapters.get_watchlist;
 
-public class GetWatchlistViewModel {
+import interface_adapters.ViewModel;
+
+import javax.swing.text.View;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class GetWatchlistViewModel extends ViewModel {
+
+    private GetWatchlistState state;
+
+    public GetWatchlistViewModel(String viewName) {
+        super(viewName);
+    }
+
+    public GetWatchlistState getState() {
+        return state;
+    }
+
+    public void setState(GetWatchlistState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
