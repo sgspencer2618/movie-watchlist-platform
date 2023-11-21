85fd64ef9ec09b6cbfbf7860478809d82ba6ff92
Alex732004 <alex.yao@mail.utoronto.ca>
2023-11-20 20:42:12 -0500

    Interface Adapters for Update Rating

--- src/interface_adapters/update_rating/UpdateRatingState.java
+++ src/interface_adapters/update_rating/UpdateRatingState.java
@@ -1,4 +1,22 @@
 package interface_adapters.update_rating;
 
+import interface_adapters.remove_rating.RemoveRatingState;
+
 public class UpdateRatingState {
+    private int newRating;
+    public UpdateRatingState(UpdateRatingState copy) {
+        newRating = copy.newRating;
+    }
+    public UpdateRatingState() {
+    }
+
+    public int getNewRating() {
+            return newRating;
+    }
+
+        public void setNewRating(int newRating) {
+            this.newRating = newRating;
+        }
+
 }
+
