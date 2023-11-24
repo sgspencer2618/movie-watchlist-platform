6c8ce43392d2dc55a76a9467f651cfaeadfcd67c
rohanbrohan <rohanbrohan@gmail.com>
2023-11-24 17:45:35 -0500

    filled out files for DB access

--- src/data_store/UserDBInterface.java
+++ src/data_store/UserDBInterface.java
@@ -1,2 +1,28 @@
-package data_store;public interface UserDBInterface {
+package data_store;
+import entity.Movie;
+import entity.User;
+
+import java.util.List;
+
+public interface UserDBInterface {
+    public User getUser(String username);
+
+    public boolean addUser(String username, String password);
+
+    public boolean userExists(String username);
+
+    public List<Movie> getWatchlist (String username);
+
+    public void addToWatchlist (String username, String movieID);
+
+    public void removeFromWatchlist (String username, String movieID);
+
+    public List<Movie> getRatings (String username);
+
+    public void addRating (String username, String movie, int rating);
+
+    public void removeRating (String username, String movie);
+
+
+
 }
