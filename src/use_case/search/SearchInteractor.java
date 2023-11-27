234454bcb78124ee9666161d57393bf4247c4362
Simon-program <simon@3450.dk>
2023-11-26 20:38:20 -0500

    Search user case setup.

--- src/use_case/search/SearchInteractor.java
+++ src/use_case/search/SearchInteractor.java
@@ -3,46 +3,37 @@ package use_case.search;
 import entity.Movie;
 import utility.ApiInterface;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
+
+import data_access.UserRatingAccessObject;
+import entity.UserRating;
 
 public class SearchInteractor implements SearchHandlerInputBoundary {
 
     final ApiInterface APICaller;
-
-    final SearchUserDataAccessInterface userDataAccessObject;
-
-    final SearchUserRatingsDataAccessInterface userRatingsDataAccessObject;
-
-    final SearchUserWatchlistDataAccessInterface userWatchlistDataAccessObject;
-
-    final  SearchHandlerOutputBoundary searchPresenter;
+    final UserRatingAccessObject userRatingAccess;
+    final SearchHandlerOutputBoundary searchPresenter;
 
 public SearchInteractor(ApiInterface APICaller,
-                        SearchUserDataAccessInterface userDataAccessInterface,
-                        SearchUserRatingsDataAccessInterface userRatingsDataAccessObject,
-                        SearchUserWatchlistDataAccessInterface userWatchlistDataAccessObject,
+                        UserRatingAccessObject userRatingAccess,
                         SearchHandlerOutputBoundary searchOutputBoundary){
     this.APICaller = APICaller;
-    this.userDataAccessObject = userDataAccessInterface;
-    this.userRatingsDataAccessObject = userRatingsDataAccessObject;
-    this.userWatchlistDataAccessObject = userWatchlistDataAccessObject;
+    this.userRatingAccess = userRatingAccess;
     this.searchPresenter = searchOutputBoundary;
 }
 
 public void execute(SearchHandlerInputData searchInputData){
-    String currUser = userDataAccessObject.getCurrUser();
+    String currUserName = searchInputData.getUser().getUsername();
     List<Movie> searchResult = APICaller.getSearch(searchInputData.getSearchQuery(), 1);
-
-    HashMap<String, Integer> userRatingsHashmap = new HashMap<>();
-    for (Movie movie : searchResult) {
-        String imdbID = movie.getImdbID();
-        Integer rating = userRatingsDataAccessObject.getUserRating(currUser, imdbID);
-
-        // if no rating, stores rating = -1
-        userRatingsHashmap.put(imdbID, rating);
+    List<UserRating> userRatings = new ArrayList<>();
+    for (Movie mov : searchResult) {
+        if (userRatingAccess.userRatingExists(currUserName, mov.getImdbID())) {
+            userRatings.add(userRatingAccess.getUserRating(currUserName, mov.getImdbID()));
+        }
     }
-
+    SearchHandlerOutputData outputData = new SearchHandlerOutputData(searchResult, userRatings);
+    searchPresenter.prepareSuccessView(outputData);
     }
 
 }
--- src/use_case/search/SearchUserRatingsDataAccessInterface.java
+++
@@ -1,6 +0,0 @@
-
-package use_case.search;
-
-public interface SearchUserRatingsDataAccessInterface {
-    int getUserRating(String user, String movieID);
-}
