c07d27f89305dd8f94fa181884d4685f5b6c78dd
Alex732004 <alex.yao@mail.utoronto.ca>
2023-12-02 03:26:36 -0500

    added comments for update rating use case

--- tests/use_case_test/update_rating/UpdateRatingInteractorTest.java
+++ tests/use_case_test/update_rating/UpdateRatingInteractorTest.java
@@ -19,6 +19,8 @@ import java.io.File;
 import static org.junit.Assert.*;
 
 public class UpdateRatingInteractorTest {
+    //Tests if a rating is properly updated. A userRating is first initialized to have a rating of 3.
+    //The interactor is called and updates the database, the new rating should be updated to 3.
     @Test
     public void testUpadateRating() {
         String imdb = "tt0345950";
@@ -37,7 +39,7 @@ public class UpdateRatingInteractorTest {
 
         UpdateRatingInputData inputData = new UpdateRatingInputData(3, username, imdb);
         UpdateRatingInputBoundary interactor = new UpdateRatingInteractor(ratingAccessObject, presenter);
-        interactor.execute(inputData); // Will send output data to successPresenter to be checked
+        interactor.execute(inputData); // Will send output data to presenter to be checked
     }
 
 }
