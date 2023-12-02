c25a6f53142047d8b75c8f242790091af190ae09
Alex732004 <alex.yao@mail.utoronto.ca>
2023-12-02 15:56:18 -0500

    added before and after

--- tests/use_case_test/remove_rating/RemoveRatingInteractorTest.java
+++ tests/use_case_test/remove_rating/RemoveRatingInteractorTest.java
@@ -3,6 +3,8 @@ package use_case_test.remove_rating;
 import entity.Movie;
 import interface_adapters.remove_rating.RemoveRatingPresenter;
 import interface_adapters.remove_rating.RemoveRatingState;
+import org.junit.After;
+import org.junit.Before;
 import use_case.movie_info.*;
 import use_case.remove_rating.*;
 import interface_adapters.remove_rating.RemoveRatingViewModel;
@@ -14,10 +16,23 @@ import entity.UserRating;
 import data_access.UserRatingAccessObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RemoveRatingInteractorTest {
+    private final String testFilePath = "./userRatingsTest.csv";
+    private List<String> originalFileContent;
+    @Before
+    public void setUp() throws IOException {
+        // Save the original file content
+        originalFileContent = Files.readAllLines(Paths.get(testFilePath));
+    }
+
+
     //Checks if a rating will be correctly deleted from database.
     //First we set up our userRating
     //We then call our interactor and it should delete the userRating object from our database.
@@ -73,6 +88,12 @@ public class RemoveRatingInteractorTest {
         RemoveRatingInputData inputData = new RemoveRatingInputData(username, imdb);
         RemoveRatingInputBoundary interactor = new RemoveRatingInteractor(ratingAccessObject, presenter);
         interactor.execute(inputData); // Will send output data to presenter to be checked
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        // Restore the original file content
+        Files.write(Paths.get(testFilePath), originalFileContent);
     }
 
 }
--- tests/use_case_test/update_rating/UpdateRatingInteractorTest.java
+++ tests/use_case_test/update_rating/UpdateRatingInteractorTest.java
@@ -3,6 +3,8 @@ package use_case_test.update_rating;
 import entity.Movie;
 import interface_adapters.update_rating.UpdateRatingPresenter;
 import interface_adapters.update_rating.UpdateRatingState;
+import org.junit.After;
+import org.junit.Before;
 import use_case.movie_info.*;
 import use_case.remove_rating.*;
 import use_case.update_rating.*;
@@ -15,10 +17,22 @@ import entity.UserRating;
 import data_access.UserRatingAccessObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UpdateRatingInteractorTest {
+    private final String testFilePath = "./userRatingsTest.csv";
+    private List<String> originalFileContent;
+    @Before
+    public void setUp() throws IOException {
+        // Save the original file content
+        originalFileContent = Files.readAllLines(Paths.get(testFilePath));
+    }
+
     //Tests if a rating is properly updated. A userRating is first initialized to have a rating of 3.
     //The interactor is called and updates the database, the new rating should be updated to 3.
     @Test
@@ -40,6 +54,12 @@ public class UpdateRatingInteractorTest {
         UpdateRatingInputData inputData = new UpdateRatingInputData(3, username, imdb);
         UpdateRatingInputBoundary interactor = new UpdateRatingInteractor(ratingAccessObject, presenter);
         interactor.execute(inputData); // Will send output data to presenter to be checked
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        // Restore the original file content
+        Files.write(Paths.get(testFilePath), originalFileContent);
     }
 
 }
